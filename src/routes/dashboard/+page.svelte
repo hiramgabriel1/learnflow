27f62a14707b9bc7ec8a7e27c32b9d7747e05152
itsrusty <rusty3031@gmail.com>
2024-02-18 14:42:20 -0700

    landing-page in progress

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-  import "../app.css";
-  import "../main.styles.css";
+  import "../../app.css";
+  import "../../main.styles.css";
 
   /**
    * @type {boolean}
@@ -406,17 +406,6 @@
         </div>
       </div>
     </div>
-    <!-- <div class="messages-section">
-  <button class="messages-close">
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle">
-      <circle cx="12" cy="12" r="10" />
-      <line x1="15" y1="9" x2="9" y2="15" />
-      <line x1="9" y1="9" x2="15" y2="15" /></svg>
-  </button>
-  <div class="projects-section-header">
-    <p>Pendientes</p>
-  </div>
-</div> -->
   </div>
 </div>
 
