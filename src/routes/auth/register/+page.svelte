503ad12957c5ba1163cf0990cd2419a6b11d4fb3
Lohana Orellano <lohanaorellano_lsi@hotmail.comgit>
2024-04-03 18:08:09 +0200

    config colors tailwind and register style

--- src/routes/auth/register/+page.svelte
+++ src/routes/auth/register/+page.svelte
@@ -85,184 +85,142 @@
 
 <Toaster />
 
-<div class="2xl:container h-screen m-auto">
-  <div hidden class="fixed inset-0 w-7/12 lg:block">
-    <!-- svelte-ignore a11y-media-has-caption -->
-    <!-- <video class="w-full h-full object-cover" loop autoplay {poster}></video> -->
+<div
+class="max-w-screen-xl m-0 sm:m-20  shadow sm:rounded-lg flex justify-center flex-1 bg-background"
+>
+<div class="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+  <div>
+   
+    <h1 class="text-2xl xl:text-3xl font-extrabold text-center text-secondary">Learnflow AI</h1>
   </div>
-  <!-- svelte-ignore a11y-unknown-role -->
-  <div
-    hidden
-    role="hidden"
-    class="fixed inset-0 w-6/12 ml-auto bg-white bg-opacity-70 backdrop-blur-xl lg:block"
-  ></div>
-  <div class="relative h-full ml-auto lg:w-6/12">
-    <div class="m-auto py-12 px-6 sm:p-20 xl:w-10/12">
-      <div class="space-y-4">
-        <p class="text-xl" style="font-size: 60px;">Learnflow AI</p>
-        <p class="font-medium text-lg text-gray-600">
-          Bienvenido a Learnflow AI. Crea una cuenta y comienza la magia!
-        </p>
-      </div>
+  <div class="mt-5 flex flex-col items-center">
+    <h4 class="text-lg lg:text-xl font-semibold text-center text-secondary">
+      ¡Bienvenido a Learnflow AI. Crea una cuenta y comienza la magia!
+      </h4>
 
-      <div class="mt-12 grid gap-6 sm:grid-cols-2">
+    <div class="w-full flex-1 mt-8">
+      <div class="flex flex-col items-center">
         <button
-          class="py-3 px-6 rounded-xl bg-blue-50 hover:bg-blue-100 focus:bg-blue-100 active:bg-blue-200"
+          class="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-secondary border-2 border-primary text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline"
         >
-          <div class="flex gap-4 justify-center">
-            <!-- <img src={google} class="w-5" alt="" /> -->
-            <button
-              class="block w-max font-medium tracking-wide text-sm text-blue-700"
-              on:click={handleAuthGoogle}>Google</button
-            >
-          </div>
-        </button>
-        <button
-          class="py-3 px-6 rounded-xl bg-gray-900 transition hover:bg-gray-800 active:bg-gray-600 focus:bg-gray-700"
-        >
-          <div class="flex gap-4 items-center justify-center text-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              class="w-5"
-              viewBox="0 0 16 16"
-            >
+          <div class="bg-white p-2 rounded-full">
+            <svg class="w-4" viewBox="0 0 533.5 544.3">
               <path
-                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+                d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
+                fill="#4285f4"
+              />
+              <path
+                d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z"
+                fill="#34a853"
+              />
+              <path
+                d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z"
+                fill="#fbbc04"
+              />
+              <path
+                d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z"
+                fill="#ea4335"
               />
             </svg>
-            <button
-              class="block w-max font-medium tracking-wide text-sm text-white"
-              on:click={handleAuthGithub}>Github</button
-            >
           </div>
+          <span class="ml-4">
+          Inicia Sesión con Google
+          </span>
+        </button>
+
+        <button
+          class="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-secondary border-2 border-primary text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5"
+        >
+          <div class="bg-white p-1 rounded-full">
+            <svg class="w-6" viewBox="0 0 32 32">
+              <path
+                fill-rule="evenodd"
+                d="M16 4C9.371 4 4 9.371 4 16c0 5.3 3.438 9.8 8.207 11.387.602.11.82-.258.82-.578 0-.286-.011-1.04-.015-2.04-3.34.723-4.043-1.609-4.043-1.609-.547-1.387-1.332-1.758-1.332-1.758-1.09-.742.082-.726.082-.726 1.203.086 1.836 1.234 1.836 1.234 1.07 1.836 2.808 1.305 3.492 1 .11-.777.422-1.305.762-1.605-2.664-.301-5.465-1.332-5.465-5.93 0-1.313.469-2.383 1.234-3.223-.121-.3-.535-1.523.117-3.175 0 0 1.008-.32 3.301 1.23A11.487 11.487 0 0116 9.805c1.02.004 2.047.136 3.004.402 2.293-1.55 3.297-1.23 3.297-1.23.656 1.652.246 2.875.12 3.175.77.84 1.231 1.91 1.231 3.223 0 4.61-2.804 5.621-5.476 5.922.43.367.812 1.101.812 2.219 0 1.605-.011 2.898-.011 3.293 0 .32.214.695.824.578C24.566 25.797 28 21.3 28 16c0-6.629-5.371-12-12-12z"
+              />
+            </svg>
+          </div>
+          <span class="ml-4">
+           Inicia Sesión con GitHub
+          </span>
         </button>
       </div>
 
-      <!-- dsklslaklñdkslñdkdslñ{dkñ{d}} -->
-      <!-- svelte-ignore a11y-unknown-role -->
-      <div role="hidden" class="mt-12 border-t">
-        <span
-          class="block w-max mx-auto -mt-3 px-4 text-center text-gray-500 bg-white"
-          >o</span
+      <div class="my-12 border-b text-center">
+        <div
+          class="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2"
         >
+          Or sign up with e-mail
+        </div>
       </div>
-      <!-- git y github_ -->
 
-      <form on:submit|preventDefault={formHandler} class="space-y-6 py-6">
-        <div class="flex flex-col">
-          <label class="pb-2 font-bold" for="">Nombres</label>
-          <input
-            type="text"
-            placeholder="Introduce tu nombre"
-            bind:value={formData.username}
-            required
-            minlength="4"
-            class="w-full py-3 px-6 ring-1 ring-gray-300 rounded-xl placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-gray-500 focus:invalid:outline-none"
-          />
-        </div>
-        <div class="flex flex-col">
-          <label class="pb-2 font-bold" for="">Apellidos</label>
-          <input
-            type="text"
-            placeholder="Introduce tu apellido"
-            bind:value={formData.lastName}
-            required
-            minlength="5"
-            class="w-full py-3 px-6 ring-1 ring-gray-300 rounded-xl placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-gray-500 focus:invalid:outline-none"
-          />
-        </div>
-        <div class="flex flex-col">
-          <label class="pb-2 font-bold" for=""
-            >Razón para usar el programa</label
+      <div class="mx-auto max-w-xs">
+        <input
+          class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+          type="password"
+          placeholder="Password"
+        />
+        <button
+          class="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+        >
+          <svg
+            class="w-6 h-6 -ml-2"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
           >
-          <input
-            type="text"
-            placeholder="Cuentanos porque quieres usar nuestro software"
-            minlength="10"
-            required
-            bind:value={formData.reasonsToUseApp}
-            class="w-full py-3 px-6 ring-1 ring-gray-300 rounded-xl placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-gray-500 focus:invalid:outline-none"
-          />
-        </div>
-
-        <!-- <div>
-          <input
-            type="text"
-            placeholder="Apellido"
-            bind:value={formData.lastName}
-            required
-            class="w-full py-3 px-6 ring-1 ring-gray-300 rounded-xl placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-red-400 focus:invalid:outline-none"
-          />
-        </div> -->
-
-        <div class="flex flex-col">
-          <label class="pb-2 font-bold" for="">Correo electronico</label>
-          <input
-            type="email"
-            placeholder="example@xyz.com"
-            bind:value={formData.userEmail}
-            required
-            class="w-full py-3 px-6 ring-1 ring-gray-300 rounded-xl placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-gray-500 focus:invalid:outline-none"
-          />
-        </div>
-
-          <div class="flex flex-col">
-          <label class="pb-2 font-bold" for="">Contraseña</label>
-          <input
-            type="password"
-            placeholder="*********"
-            bind:value={formData.password}
-            required
-            class="w-full py-3 px-6 ring-1 ring-gray-300 rounded-xl placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-gray-500 focus:invalid:outline-none"
-          />
-        </div>
-
-        <!-- <div>
-          <select
-            bind:value={formData.isStudent}
-            required
-            class="w-full py-3 px-6 ring-1 ring-gray-300 rounded-xl placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-red-400 focus:invalid:outline-none"
-          >
-            <option value="" disabled selected>¿Eres estudiante?</option>
-            <option value="true">Sí</option>
-            <option value="false">No</option>
-          </select>
-        </div> -->
-
-        <div class="flex flex-col items-end">
-          <!-- <input
-            type="text"
-            placeholder="¿Por que quieres usar LearnflowAI?"
-            bind:value={formData.reasonsToUseApp}
-            required
-            class="w-full py-3 px-6 ring-1 ring-gray-300 rounded-xl placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-red-400 focus:invalid:outline-none"
-          /> -->
-        </div>
-
-        <div>
-          <button
-            class="w-full px-6 py-3 rounded-full bg-sky-500 transition hover:bg-sky-600 focus:bg-sky-600 active:bg-sky-800"
-          >
-            <span class="font-semibold text-white text-lg">Crear cuenta</span>
-          </button>
-
-          <div class="flex justify-between">
-            <a href="/auth/login" type="reset" class="w-max p-3 -ml-3">
-              <span class="text-sm tracking-wide text-blue-600"
-                >Ya tengo una cuenta</span
-              >
-            </a>
-            <button type="reset" class="w-max p-3 -mr-3">
-              <span class="text-sm tracking-wide text-blue-600"
-                >Olvidé mi contraseña
-              </span>
-            </button>
-          </div>
-        </div>
-      </form>
+            <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+            <circle cx="8.5" cy="7" r="4" />
+            <path d="M20 8v6M23 11h-6" />
+          </svg>
+          <span class="ml-3">
+            Sign Up
+          </span>
+        </button>
+        <p class="mt-6 text-xs text-gray-600 text-center">
+          I agree to abide by templatana's
+          <a href="#" class="border-b border-gray-500 border-dotted">
+            Terms of Service
+          </a>
+          and its
+          <a href="#" class="border-b border-gray-500 border-dotted">
+            Privacy Policy
+          </a>
+        </p>
+      </div>
     </div>
   </div>
 </div>
+<div class="flex-1 bg-indigo-100 text-center hidden lg:flex">
+  <div
+    class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
+    style="background-image: url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg');"
+  ></div>
+</div>
+</div>
+<div class="REMOVE-THIS-ELEMENT-IF-YOU-ARE-USING-THIS-PAGE hidden treact-popup fixed inset-0 flex items-center justify-center" style="background-color: rgba(0,0,0,0.3);">
+  <div class="max-w-lg p-8 sm:pb-4 bg-white rounded shadow-lg text-center sm:text-left">
+    
+    <h3 class="text-xl sm:text-2xl font-semibold mb-6 flex flex-col sm:flex-row items-center">
+      <div class="bg-green-200 p-2 rounded-full flex items-center mb-4 sm:mb-0 sm:mr-2">
+        <svg class="text-green-800 inline-block w-5 h-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"></path></svg>
+        </div>
+      Free TailwindCSS Component Kit!
+    </h3>  
+    <p>I recently released Treact, a <span class="font-bold">free</span> TailwindCSS Component Kit built with React.</p>
+    <p class="mt-2">It has 52 different UI components, 7 landing pages, and 8 inner pages prebuilt. And they are customizable!</p>
+    <div class="mt-8 pt-8 sm:pt-4 border-t -mx-8 px-8 flex flex-col sm:flex-row justify-end leading-relaxed">
+      <button class="close-treact-popup px-8 py-3 sm:py-2 rounded border border-gray-400 hover:bg-gray-200 transition duration-300">Close</button>
+      <a class="font-bold mt-4 sm:mt-0 sm:ml-4 px-8 py-3 sm:py-2 rounded bg-purple-700 text-gray-100 hover:bg-purple-900 transition duration-300 text-center" href="https://treact.owaiskhan.me" target="_blank">See Treact</a>
+    </div>
+  </div>
+</div>
+
 
 <!-- tailwind -->
