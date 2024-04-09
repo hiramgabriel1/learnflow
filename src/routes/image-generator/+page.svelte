3259bedf3efc358a79d62440cd1eff58a4d1e40f
itsrusty <rusty3031@gmail.com>
2024-04-09 09:39:48 -0700

    comment provider paint

--- src/routes/image-generator/+page.svelte
+++ src/routes/image-generator/+page.svelte
@@ -154,7 +154,7 @@
             class="flex h-10 w-fit rounded-md border border-input  px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
           >
             <option value="realistic">Realismo</option>
-            <option value="paint">Pintura</option>
+            <!-- <option value="paint">Pintura</option> -->
             <option value="cartoon">Caricatura</option>
           </select>
         </div>
